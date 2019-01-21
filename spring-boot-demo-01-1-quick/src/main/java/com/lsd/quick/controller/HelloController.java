280f13e51a360252ffe48f57331acfcf8c6a05b8
lsd2016 <1157718866@qq.com>
2019-01-21 15:49:11 +0800

    Initial commit

---
+++ spring-boot-demo-01-1-quick/src/main/java/com/lsd/quick/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.lsd.quick.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description:
+ * @author: lishengdong
+ * @create: 2019/1/21 15:45
+ */
+@RestController
+public class HelloController {
+    /**
+     * 访问：http://localhost:8080/hello
+     * @return
+     */
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello world quick!";
+    }
+}
