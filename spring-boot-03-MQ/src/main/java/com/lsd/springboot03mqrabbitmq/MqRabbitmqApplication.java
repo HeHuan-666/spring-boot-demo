6797654a0da553c3d5f332a4fa212f5d6f21c613
lsd2016 <1157718866@qq.com>
2019-01-31 18:53:57 +0800

    20100131：RESTfull API简单项目的快速搭建

---
+++ spring-boot-03-MQ/src/main/java/com/lsd/springboot03mqrabbitmq/MqRabbitmqApplication.java
@@ -0,0 +1,14 @@
+package com.lsd.springboot03mqrabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqRabbitmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MqRabbitmqApplication.class, args);
+    }
+
+}
+
